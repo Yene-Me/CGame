6b809e00201e87202155bab1e548f83dbb5ba71c
Lewis <lewisq94@gmail.com>
2017-06-17 19:21:35 +0100

    Animate game over screen
    //TODO: add some social sharing

--- public/js/util/GameOverUpdate.js
+++ public/js/util/GameOverUpdate.js
@@ -3,12 +3,12 @@ export default class GameOverUpdate
   constructor()
   {
     this.counter = document.createElement("DIV");
-    this.counter.classList.add("gameover");
     document.body.appendChild(this.counter);
   }
 
   showMessage()
   {
+      this.counter.classList.add("gameover");
     this.counter.textContent = "Game Over";
     this.reload = document.createElement("DIV");
     this.counter.appendChild(this.reload);
