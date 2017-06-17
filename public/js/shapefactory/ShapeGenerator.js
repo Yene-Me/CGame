8b3f4d967dd81ce5f559fee02b8a6ba61f617759
Lewis <lewisq94@gmail.com>
2017-06-17 01:52:03 +0100

    Tweaks

--- public/js/shapefactory/ShapeGenerator.js
+++ public/js/shapefactory/ShapeGenerator.js
@@ -4,251 +4,257 @@ import Lights from '../util/Lights';
 import CubeLocation from '../util/CubeLocation';
 import * as TWEEN from 'tween.js';
 
-export default class ShapeGenerator
-{
-  constructor(uiElement)
-  {
-      this.TOTAL_CUBE_ITEMS = 7;
-      this._shapeHolder = uiElement;
-      this.scene = null;
-      this.camera = null;
-      this.renderer = null;
-      this.geometry = null;
-      this.material = null;
-      this.mesh = null;
-      this.size = SizeFinder.CUBE_SIZE;
-      this.raycaster = new THREE.Raycaster();
-      this.mouse = new THREE.Vector2();
-      this.isCheckInProgress = false;
-      this.randomColour = [];
-      this.boxColour = [];
+export default class ShapeGenerator {
+    constructor(uiElement) {
+        this.TOTAL_CUBE_ITEMS = 7;
+        this._shapeHolder = uiElement;
+        this.scene = null;
+        this.camera = null;
+        this.renderer = null;
+        this.geometry = null;
+        this.material = null;
+        this.mesh = null;
+        this.size = SizeFinder.CUBE_SIZE;
+        this.raycaster = new THREE.Raycaster();
+        this.mouse = new THREE.Vector2();
+        this.isCheckInProgress = false;
+        this.randomColour = [];
+        this.boxColour = [];
 
-      this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-      this.renderer.setSize( window.innerWidth, window.innerHeight );
-      this.renderer.setPixelRatio((window.devicePixelRatio) ? window.devicePixelRatio : 1);
+        this.renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        this.renderer.setPixelRatio((window.devicePixelRatio) ? window.devicePixelRatio : 1);
 
-      this.renderer.autoClear = false;
-      this.renderer.setClearColor(0x000000, 0.0);
+        this.renderer.autoClear = false;
+        this.renderer.setClearColor(0x000000, 0.0);
 
-      this._shapeHolder.appendChild( this.renderer.domElement);
+        this._shapeHolder.appendChild(this.renderer.domElement);
 
-      this.scene = new THREE.Scene();
+        this.scene = new THREE.Scene();
 
-      this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
-      this.camera.position.z = 1000;
-      this.createColourSet();
-      this.createRandomNumber();
-      this.addLight();
-      this._cubeLocation = new CubeLocation(this.TOTAL_CUBE_ITEMS,250);
+        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
+        this.camera.position.z = 1000;
+        this.createColourSet();
+        this.createRandomNumber();
+        this.addLight();
+        this._cubeLocation = new CubeLocation(this.TOTAL_CUBE_ITEMS, 250);
 
-      this._cubeCollection = [];
-  }
-
-  createColourSet()
-  {
-    for(let index = 0; index < 10 ; index++)
-    {
-      this.boxColour.push (Lights.SET_OF_COLOURS)
+        this._cubeCollection = [];
     }
 
-  }
+    createColourSet() {
+        for (let index = 0; index < 10; index++) {
+            this.boxColour.push(Lights.SET_OF_COLOURS)
+        }
 
-  createRandomNumber()
-  {
-    for (var a = [0,1,2,3,4,5,6,7], i = a.length; i-- ;) {
-      var random = a.splice(Math.floor(Math.random() * (i + 1)), 1)[0];
-      this.randomColour.push(random);
     }
 
-  }
+    createRandomNumber() {
+        for (var a = [0, 1, 2, 3, 4, 5, 6, 7], i = a.length; i--;) {
+            var random = a.splice(Math.floor(Math.random() * (i + 1)), 1)[0];
+            this.randomColour.push(random);
+        }
 
-  loadCube()
-  {
-    this.main = this.createBoxItem(0,400,0,this.boxColour[0]);
-
-    //this.firstItem = this.createBoxItem((this.size*2)*-1,-200, 0 ,this.boxColour [this.randomColour[0]]);
-    //this.secondItem = this.createBoxItem(0, -200, 0,this.boxColour [this.randomColour[1]]);
-    //this.thirdItem = this.createBoxItem((this.size*2),-200,0,this.boxColour [this.randomColour[2]]);
-    var index = 0
-    for(let item of this._cubeLocation)
-    {
-      let cubeData = {
-        cube:this.createBoxItem(item.x,-200,item.z,this.boxColour[this.randomColour[index]]),
-        angle:item.angel,
-        rotation : {
-          x: Math.random()/50,
-          y: Math.random()/50,
-          z: Math.random()/50
-      }
     }
-      index++;
-      this._cubeCollection.push(cubeData);
+
+    loadCube() {
+        this.main = this.createBoxItem(0, 400, 0, this.boxColour[0]);
+
+        //this.firstItem = this.createBoxItem((this.size*2)*-1,-200, 0 ,this.boxColour [this.randomColour[0]]);
+        //this.secondItem = this.createBoxItem(0, -200, 0,this.boxColour [this.randomColour[1]]);
+        //this.thirdItem = this.createBoxItem((this.size*2),-200,0,this.boxColour [this.randomColour[2]]);
+        var index = 0
+        for (let item of this._cubeLocation) {
+            let cubeData = {
+                cube: this.createBoxItem(item.x, -200, item.z, this.boxColour[this.randomColour[index]]),
+                angle: item.angel,
+                rotation: {
+                    x: Math.random() / 50,
+                    y: Math.random() / 50,
+                    z: Math.random() / 50
+                }
+            }
+            index++;
+            this._cubeCollection.push(cubeData);
+        }
     }
-  }
 
-  reloadCube()
-  {
-    this.main.position.set(0,400,0);
-    //this.firstItem.position.set((this.size*2)*-1,-200,0);
-    //this.secondItem.position.set(0,-200,0);
-    //this.thirdItem.position.set((this.size*2),-200,0);
+    reloadCube() {
+        this.main.position.set(0, 400, 0);
+        //this.firstItem.position.set((this.size*2)*-1,-200,0);
+        //this.secondItem.position.set(0,-200,0);
+        //this.thirdItem.position.set((this.size*2),-200,0);
 
-    for(let index = 0 ; index < this._cubeLocation.length ; index++)
-    {
-      let item = this._cubeLocation[index];
-      this._cubeCollection[index].cube.position.set(item.x,-200,item.z);
+        for (let index = 0; index < this._cubeLocation.length; index++) {
+            let item = this._cubeLocation[index];
+            this._cubeCollection[index].cube.position.set(item.x, -200, item.z);
+        }
     }
-  }
 
-  addLight()
-  {
-    this.scene.add(Lights.ADD_LIGHTS[0]);
-    this.scene.add(Lights.ADD_LIGHTS[1]);
-    this.scene.add(Lights.ADD_LIGHTS[2]);
-  }
+    addLight() {
+        this.scene.add(Lights.ADD_LIGHTS[0]);
+        this.scene.add(Lights.ADD_LIGHTS[1]);
+        this.scene.add(Lights.ADD_LIGHTS[2]);
+        this.scene.add(Lights.ADD_LIGHTS[3]);
+    }
 
-  createBoxItem(x,y,z,colour)
-  {
-     let geometry = new THREE.BoxGeometry( this.size, this.size, this.size );
+    createBoxItem(x, y, z, colour) {
+        let geometry = new THREE.BoxGeometry(this.size, this.size, this.size);
 
-     let material = [new THREE.MeshLambertMaterial( { color: colour[0], wireframe: false } ),
-     new THREE.MeshLambertMaterial( { color:  colour[1], wireframe: false } ),
-        new THREE.MeshLambertMaterial( { color: colour[2], wireframe: false } ),
-        new THREE.MeshLambertMaterial( { color: colour[3], wireframe: false } ),
-        new THREE.MeshLambertMaterial( { color: colour[4], wireframe: false } ),
-        new THREE.MeshLambertMaterial( { color: colour[5], wireframe: false } )
+        let material = [
+            new THREE.MeshStandardMaterial({color: colour[0], metalness: 0.1}),
+            new THREE.MeshStandardMaterial({color: colour[1], metalness: 0.1}),
+            new THREE.MeshStandardMaterial({color: colour[2], metalness: 0.1}),
+            new THREE.MeshStandardMaterial({color: colour[3], metalness: 0.1}),
+            new THREE.MeshStandardMaterial({color: colour[4], metalness: 0.1}),
+            new THREE.MeshStandardMaterial({color: colour[5], metalness: 0.1})
         ];
 
-     let mesh = new THREE.Mesh( geometry, material );
+        let mesh = new THREE.Mesh(geometry, material);
 
-     mesh.position.y = y;
-     mesh.position.x = x;
-     mesh.position.z = z;
-     this.scene.add(mesh);
-     return mesh;
-  }
-
-  onMouseMove( event )
-  {
-  	 this.mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-  	 this.mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
-  }
-
-  update()
-  {
-    if(this.isCheckInProgress)
-    {
-      return;
+        mesh.position.y = y;
+        mesh.position.x = x;
+        mesh.position.z = z;
+        this.scene.add(mesh);
+        return mesh;
     }
 
-  	this.raycaster.setFromCamera( this.mouse, this.camera );
-
-  	// calculate objects intersecting the picking ray
-  	var intersects = this.raycaster.intersectObjects( this.scene.children );
-
-  	for ( var i = 0; i < intersects.length; i++ )
-    {
-  		//intersects[ i ].object.material[0].color.set( 0x000000 );
-      this.moveToPoint(intersects[ i ].object);
-  	}
-
-}
-
-moveToPoint(object)
-{
-  let item = object;
-  this.isCheckInProgress = true;
-
-  let itemx = item["position"].x;
-  let itemy = item["position"].y;
-
-  let tween = new TWEEN.Tween( { x: itemx, y: itemy , scale: 1 } )
-      .to( { x: 0 , y:100, scale:0.8 }, 2000 )
-      .easing( TWEEN.Easing.Elastic.InOut )
-
-      .onUpdate( function () {
-          item["position"].x = this.x;
-          item["position"].y = this.y;
-      } ).start().onComplete(()=>{
-            this.checkPickedWithMain(item);
-      });
-}
-
-checkPickedWithMain(selectedObject)
-{
-
-  selectedObject.rotation.y = 0;
-  selectedObject.rotation.x = 0;
-  selectedObject.rotation.z = 0;
-
-  this.main.rotation.x = 0;
-  this.main.rotation.y = 0;
-  this.main.rotation.z = 0;
-  this.isCompareOn = true;
-
-  let main = this.main;
-
-
-  let tween = new TWEEN.Tween( { x: 0, y: 0 , z: 0 } )
-      .to( { x:0.08 , y:0.05, z:1 }, 500 )
-      .easing( TWEEN.Easing.Elastic.InOut )
-      .repeat(5)
-      .onUpdate( function () {
-        selectedObject.rotation.x += this.x;
-        main.rotation.x += this.x;
-
-      } ).start().onComplete(()=>{
-          //this.mouse = null;
-          this.mouse.x = 0;
-          this.mouse.y = 0;
-          this.reloadCube();
-          this.isCheckInProgress = false;
-          this.isCompareOn = false;
-      });
-}
-
-animate()
- {
-  requestAnimationFrame( ()=>{
-    this.animate()
-  } );
-
-    if(!this.isCompareOn)
-    {
-    this.main.rotation.y -=0.01;
-    this.main.rotation.x -=0.02;
-    this.main.rotation.z -=0.02;
-
-    //this.firstItem.rotation.y -=0.01;
-    //this.firstItem.rotation.x -=0.02;
-    //this.firstItem.rotation.z -=0.02;
-
-    //this.secondItem.rotation.y -=0.02;
-    //this.secondItem.rotation.x +=0.01;
-    //this.secondItem.rotation.z +=0.01;
-
-
-    //this.thirdItem.rotation.y +=0.01;
-    //this.thirdItem.rotation.x -=0.02;
-    //this.thirdItem.rotation.z +=0.01;
-
-    for(let item of this._cubeCollection)
-    {
-      item.cube.rotation.y +=item.rotation.x;
-      item.cube.rotation.x -=item.rotation.y;
-      item.cube.rotation.z +=item.rotation.z;
-
-      item.cube.position.z = Math.cos(item.angle)*300;
-      item.cube.position.x = Math.sin(item.angle)*300;
-
-      item.angle+=0.02;
+    onMouseMove(event) {
+        this.mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+        this.mouse.y = -( event.clientY / window.innerHeight ) * 2 + 1;
     }
 
-  }
+    update() {
+        if (this.isCheckInProgress) {
+            return;
+        }
 
-    this.update();
-    TWEEN.update();
-    this.renderer.render( this.scene, this.camera );
+        this.raycaster.setFromCamera(this.mouse, this.camera);
 
-  }
+        // calculate objects intersecting the picking ray
+        var intersects = this.raycaster.intersectObjects(this.scene.children);
+
+        for (var i = 0; i < intersects.length; i++) {
+            //intersects[ i ].object.material[0].color.set( 0x000000 );
+            this.moveToPoint(intersects[i].object);
+        }
+
+    }
+
+    moveToPoint(object) {
+        let item = object;
+        this.isCheckInProgress = true;
+
+        let itemx = item["position"].x;
+        let itemy = item["position"].y;
+
+        let tween = new TWEEN.Tween({
+            x: itemx,
+            y: itemy,
+            scale: 1,
+            rx: item.rotation.x,
+            ry: item.rotation.y,
+            rz: item.rotation.z
+        })
+            .to({x: 0, y: 100, scale: 2, rx: 0, ry: 0, rz: 0}, 2000)
+            .easing(TWEEN.Easing.Elastic.InOut)
+
+            .onUpdate(function () {
+                item["position"].x = this.x;
+                item["position"].y = this.y;
+                item["rotation"].x = this.rx;
+                item["rotation"].y = this.ry;
+                item["rotation"].z = this.rz;
+            }).start().onComplete(() => {
+                this.checkPickedWithMain(item);
+            });
+
+
+        let main = this.main;
+        let AlignTween = new TWEEN.Tween({x: main.rotation.x, y: main.rotation.y, z: main.rotation.z})
+            .to({x: 0, y: 0, z: 0}, 2000)
+            .easing(TWEEN.Easing.Back.Out)
+
+            .onUpdate(function () {
+                main["rotation"].x = this.x;
+                main["rotation"].y = this.y;
+                main["rotation"].z = this.z;
+            }).start();
+
+
+    }
+
+    checkPickedWithMain(selectedObject) {
+
+        selectedObject.rotation.y = 0;
+        selectedObject.rotation.x = -0.02;
+        selectedObject.rotation.z = 0;
+
+        this.main.rotation.x = 0;
+        this.main.rotation.y = 0;
+        this.main.rotation.z = 0;
+        this.isCompareOn = true;
+
+        let main = this.main;
+
+
+        let tween = new TWEEN.Tween({x: 0, y: 0, z: 0})
+            .to({x: 0.08, y: 0.05, z: 1}, 500)
+            .easing(TWEEN.Easing.Elastic.InOut)
+            .repeat(5)
+            .onUpdate(function () {
+                selectedObject.rotation.x += this.x;
+                main.rotation.x += this.x;
+
+            }).start().onComplete(() => {
+                //this.mouse = null;
+                this.mouse.x = 0;
+                this.mouse.y = 0;
+                this.reloadCube();
+                this.isCheckInProgress = false;
+                this.isCompareOn = false;
+            });
+    }
+
+    animate() {
+        requestAnimationFrame(() => {
+            this.animate()
+        });
+
+        if (!this.isCompareOn) {
+            this.main.rotation.y -= 0.01;
+            this.main.rotation.x -= 0.02;
+            this.main.rotation.z -= 0.02;
+
+            //this.firstItem.rotation.y -=0.01;
+            //this.firstItem.rotation.x -=0.02;
+            //this.firstItem.rotation.z -=0.02;
+
+            //this.secondItem.rotation.y -=0.02;
+            //this.secondItem.rotation.x +=0.01;
+            //this.secondItem.rotation.z +=0.01;
+
+
+            //this.thirdItem.rotation.y +=0.01;
+            //this.thirdItem.rotation.x -=0.02;
+            //this.thirdItem.rotation.z +=0.01;
+
+            for (let item of this._cubeCollection) {
+                item.cube.rotation.y += item.rotation.x;
+                item.cube.rotation.x -= item.rotation.y;
+                item.cube.rotation.z += item.rotation.z;
+
+                item.cube.position.z = Math.cos(item.angle) * 300;
+                item.cube.position.x = Math.sin(item.angle) * 300;
+
+                item.angle += 0.01;
+            }
+
+        }
+
+        this.update();
+        TWEEN.update();
+        this.renderer.render(this.scene, this.camera);
+
+    }
 }
--- public/js/util/Lights.js
+++ public/js/util/Lights.js
@@ -6,24 +6,25 @@ export default class Light{
   static get ADD_LIGHTS()
   {
     let lights = [];
-    lights[0] = new THREE.DirectionalLight( 0xffffff, 1 );
-    lights[0].position.set( 0, 1, 0 );
-    lights[0].castShadow = true;
+      lights.push(new THREE.AmbientLight( 0xFFFFFF, .5 )); // soft white light
 
-    lights[1] = new THREE.DirectionalLight( 0xffffff, 1 );
-    lights[1].position.set( 1, 0, 0 );
+    lights[1] = new THREE.DirectionalLight( 0xffffff, .5 );
+    lights[1].position.set( 0, 10, 0 );
     lights[1].castShadow = true;
 
-    lights[2] = new THREE.DirectionalLight( 0xffffff, 1 );
-    lights[2].position.set( 0, 0, 1 );
+    lights[2] = new THREE.DirectionalLight( 0xffffff, .5 );
+    lights[2].position.set( 10, 0, 0 );
     lights[2].castShadow = true;
+
+    lights[3] = new THREE.DirectionalLight( 0xffffff, .5 );
+    lights[3].position.set( 0, 0, 10 );
+    lights[3].castShadow = true;
 
     return lights;
   }
 
   static get COLOURS()
   {
-
     let colourBank = [];
 
     for(let index = 0; index < 3 ; index++)
@@ -32,7 +33,7 @@ export default class Light{
 
        for(let j = 0 ; j < 6 ; j ++)
        {
-         colourSet.push("#"+Math.floor(Math.random()*16777215).toString(16))
+         colourSet.push(Light.SET_OF_COLOURS)
        }
 
        colourBank.push(colourSet);
@@ -48,7 +49,7 @@ export default class Light{
 
         for(let j = 0 ; j < 6 ; j ++)
         {
-          colourBank.push("#"+Math.floor(Math.random()*16777215).toString(16))
+          colourBank.push(Light.GET_RANDOM_COLOUR())
         }
 
         for(let j = 0 ; j < 6 ; j ++)
@@ -56,6 +57,25 @@ export default class Light{
             colourSet.push(colourBank[j%3]);
         }
         return colourSet;
+  }
+
+  static GET_RANDOM_COLOUR()
+  {
+      return Light.COLOUR_LIST[Math.floor(Math.random()*Light.COLOUR_LIST.length)];
+  }
+
+  static get COLOUR_LIST()
+  {
+      return [
+      "#E91E63",
+      "#9C27B0",
+      "#3F51B5",
+      "#2196F3",
+      "#4CAF50",
+      "#FFEB3B",
+      "#FF9800",
+      "#FF5722",
+      ]
   }
 
 }
